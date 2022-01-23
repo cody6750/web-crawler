1b468df4ce0e56852fca4bd88f3347b639fa1293
cody <cody6750@gmail.com>
2022-01-22 17:36:40 -0800

    add graceful web server shutdown

--- webServer/webServer.go
+++ webServer/webServer.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/cody6750/codywebapi/webServer/handler"
 )
@@ -12,15 +15,35 @@ func main() {
 	// Given a request on the http server, based on the path, a specific function is ran. Example  curl -v localhost:9090/goodbye will run the function parameter
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	Hello := handler.NewHello(l)
+	GoodBye := handler.NewGoodBye(l)
 	sm := http.NewServeMux()
 	sm.Handle("/", Hello)
-	sm.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Print("good bye world")
-	})
+	sm.Handle("/goodbye", GoodBye)
 
 	//Function for creating a web service on port :9090. Called by curl -v localhost:9090
-	http.ListenAndServe(":9090", sm)
-	log.Print("done")
+	server := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  time.Second * 120,
+		ReadTimeout:  time.Second * 1,
+		WriteTimeout: time.Second * 1,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	l.Print("Recieved teriminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc)
+
 }
 
 func tesT() {
